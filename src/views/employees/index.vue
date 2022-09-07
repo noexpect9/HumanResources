22c731b36409cef382fea291145234e4d384abd5
烂泥 <1909293822@qq.com>
2022-09-07 16:37:10 +0800

    完成excel导出

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -7,8 +7,15 @@
           <span slot="before">共{{ page.total }}条记录</span>
         </template>
         <template slot="after">
-          <el-button size="small" type="warning" @click="$router.push('/import')">导入</el-button>
-          <el-button size="small" type="danger">导出</el-button>
+          <el-button
+            size="small"
+            type="warning"
+            @click="$router.push('/import')"
+            >导入</el-button
+          >
+          <el-button size="small" type="danger" @click="exportExcel"
+            >导出</el-button
+          >
 
           <el-button size="small" type="primary" @click="showDialog = true"
             >新增员工</el-button
@@ -83,6 +90,7 @@ import { getStaffListAPI, delStaffAPI } from '@/api/employees'
 import employeeEnum from '@/api/constant/employees'
 // 引用子组件
 import addStaff from './components/addStaff.vue'
+import { formatDate } from '@/filters'
 export default {
   components: {
     addStaff
@@ -130,6 +138,44 @@ export default {
         console.log(error);
       }
     },
+    exportExcel() {
+      const headers = {
+        '姓名': 'username',
+        '手机号': 'mobile',
+        '入职日期': 'timeOfEntry',
+        '聘用形式': 'formOfEmployment',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName'
+      }
+      import('@/vendor/Export2Excel').then(async excel => {
+        // 获取员工的页码 总数
+        const { rows } = await getStaffListAPI({ page: 1, size: this.page.total })
+        // rows为员工信息 是一个对象 需要转换为数组
+        const data = this.formatJson(headers, rows)   // 顺序结构要跟header对应
+        excel.export_json_to_excel({
+          header: Object.keys(headers), // 表头
+          data, // 数组
+          filename: '员工基础信息表',
+          bookType: 'xlsx',
+          autoWidth: true
+        })
+      })
+    },
+    formatJson(headers, rows) {
+      return rows.map(item => { // item为一个对象
+        return Object.keys(headers).map(key => {  // 根据headers转换
+          if (headers[key] === 'timeOfEntry' || headers[key] === 'correctionTime') {
+            return formatDate(item[headers[key]])
+          } else if (headers[key] === 'formOfEmployment') {
+            const obj = employeeEnum.hireType.find(obj => obj.id === item[headers[key]])
+            return obj ? obj.value : '未知'
+          }
+          return item[headers[key]]  //  headers[key]拿到'username'... item[headers[key]]拿到value 
+        })
+      })
+      // return rows.map(item => Object.keys(headers).map(key => item[headers[key]]))
+    }
   }
 }
 </script>
