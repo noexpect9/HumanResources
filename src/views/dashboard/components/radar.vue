b336631ac027121a8df4e3973b74cc7c2476d784
烂泥 <1909293822@qq.com>
2022-09-11 23:30:36 +0800

    完成首页

---
+++ src/views/dashboard/components/radar.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- 雷达图  图表必须给高和宽度-->
+  <div ref="myDiv" class="radar-echart" />
+</template>
+
+<script>
+// 完成加载过程
+const echarts = require('echarts/lib/echarts') // 引入echarts主模块
+require('echarts/lib/chart/radar') // 引入雷达图
+// 引入提示框和标题组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
+require('echarts/components')
+
+export default {
+  // 页面渲染完毕事件
+  mounted() {
+    const myChart = echarts.init(this.$refs.myDiv) // 得到图表实例
+    myChart.setOption({
+      title: {
+        text: 'Basic Radar Chart'
+      },
+      tooltip: {},
+      legend: {
+        data: ['预算分配(Allocated Budget)', '实际开销(Actual Spending)']
+      },
+      radar: {
+        // shape: 'circle',
+        indicator: [
+          { name: 'Sales', max: 6500 },
+          { name: 'Administration', max: 16000 },
+          { name: 'Information Technology', max: 30000 },
+          { name: 'Customer Support', max: 38000 },
+          { name: 'Development', max: 52000 },
+          { name: 'Marketing', max: 25000 }
+        ]
+      },
+      series: [
+        {
+          name: 'Budget vs spending',
+          type: 'radar',
+          data: [
+            {
+              value: [4200, 3000, 20000, 35000, 50000, 18000],
+              name: 'Allocated Budget'
+            },
+            {
+              value: [5000, 14000, 28000, 26000, 42000, 21000],
+              name: 'Actual Spending'
+            }
+          ]
+        }
+      ]
+    })
+  }
+}
+</script>
+
+<style>
+.radar-echart {
+  width: 600px;
+  height: 400px;
+}
+</style>
